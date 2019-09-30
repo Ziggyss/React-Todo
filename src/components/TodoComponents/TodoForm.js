bf7917680bd86b184ed224ee64e11c5475e7a969
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-09-30 18:01:47 +0100

    refactor TodoForm to make it a class component

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,22 +1,24 @@
 import React from "react";
 
-function TodoForm({ onChange, addTodo, clearCompleted, newTodo }) {
-  return (
-    <div className="formContainer">
-      <div>
-        <input
-          value={newTodo}
-          onChange={onChange}
-          type="text"
-          placeholder="Enter a task"
-        ></input>
+class TodoForm extends React.Component {
+  render() {
+    return (
+      <div className="formContainer">
+        <div>
+          <input
+            value={this.props.newTodo}
+            onChange={this.props.onChange}
+            type="text"
+            placeholder="Enter a task"
+          ></input>
+        </div>
+        <div>
+          <button onClick={this.props.addTodo}>Add Todo</button>
+          <button onClick={this.props.clearCompleted}>Clear Completed</button>
+        </div>
       </div>
-      <div>
-        <button onClick={addTodo}>Add Todo</button>
-        <button onClick={clearCompleted}>Clear Completed</button>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default TodoForm;
